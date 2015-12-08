45408152d5fd325eaee76beefd6e448e6f9331ac
sftruelx <truelx@outlook.com>
2015-12-08 14:40:40 +0800

    Initial commit

--- demo/stock/src/main/java/it/gmariotti/cardslib/demo/ui/TestActivity.java
+++ demo/stock/src/main/java/it/gmariotti/cardslib/demo/ui/TestActivity.java
@@ -18,12 +18,24 @@
 
 package it.gmariotti.cardslib.demo.ui;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import it.gmariotti.cardslib.demo.R;
+import it.gmariotti.cardslib.demo.fragment.NativeDashFragment;
+import it.gmariotti.cardslib.demo.fragment.nativeview.NativeCardFragment;
+import it.gmariotti.cardslib.demo.fragment.nativeview.NativeCardWithListFragment;
+import it.gmariotti.cardslib.demo.fragment.nativeview.NativeChangeValueCardFragment;
+import it.gmariotti.cardslib.demo.fragment.nativeview.NativeHeaderFragment;
+import it.gmariotti.cardslib.demo.fragment.nativeview.NativeListBaseFragment;
+import it.gmariotti.cardslib.demo.fragment.nativeview.NativeMaterialCardFragment;
 
 /**
  * @author Gabriele Mariotti (gabri.mariotti@gmail.com)
@@ -44,7 +56,62 @@ public class TestActivity extends BaseActivity  {
         }
 
         setContentView(R.layout.carddemo_activity_test);
-
+       if (null == savedInstanceState) {
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.container, new NativeMaterialCardFragment())
+                    .commit();
+        }
+        LinearLayout linear = (LinearLayout)findViewById(R.id.piclist);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 200);
+        params.setMargins(20, 20, 20, 20);
+        ImageView img=new ImageView(this);
+        img.setMaxHeight(200);
+        img.setMaxWidth(200);
+        img.setAdjustViewBounds(true);
+        img.setImageResource(R.drawable.im_beach);
+        img.setScaleType(ImageView.ScaleType.CENTER);
+        img.setLayoutParams(params);
+        linear.addView(img);
+        ImageView img5=new ImageView(this);
+        img5.setMaxHeight(200);
+        img5.setMaxWidth(200);
+        img5.setAdjustViewBounds(true);
+        img5.setImageResource(R.drawable.im_beach);
+        img5.setScaleType(ImageView.ScaleType.CENTER);
+        img5.setLayoutParams(params);
+        linear.addView(img5);
+        ImageView img0=new ImageView(this);
+        img0.setMaxHeight(200);
+        img0.setMaxWidth(200);
+        img0.setAdjustViewBounds(true);
+        img0.setImageResource(R.drawable.im_beach);
+        img0.setScaleType(ImageView.ScaleType.CENTER);
+        img0.setLayoutParams(params);
+        linear.addView(img0);
+        ImageView img1=new ImageView(this);
+        img1.setMaxHeight(200);
+        img1.setMaxWidth(200);
+        img1.setAdjustViewBounds(true);
+        img1.setImageResource(R.drawable.im_beach);
+        img1.setScaleType(ImageView.ScaleType.CENTER);
+        img1.setLayoutParams(params);
+        linear.addView(img1);
+        ImageView img2=new ImageView(this);
+        img2.setMaxHeight(200);
+        img2.setMaxWidth(200);
+        img2.setAdjustViewBounds(true);
+        img2.setImageResource(R.drawable.im_beach);
+        img2.setScaleType(ImageView.ScaleType.CENTER);
+        img2.setLayoutParams(params);
+        linear.addView(img2);
+        ImageView img3=new ImageView(this);
+        img3.setMaxHeight(200);
+        img3.setMaxWidth(200);
+        img3.setAdjustViewBounds(true);
+        img3.setImageResource(R.drawable.im_beach);
+        img3.setScaleType(ImageView.ScaleType.CENTER);
+        img3.setLayoutParams(params);
+        linear.addView(img3);
         TextView tx = (TextView) findViewById(R.id.guidelines_text);
         tx.setText(R.string.test_text);
         tx.setMovementMethod(new LinkMovementMethod());
